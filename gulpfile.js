28ab0079001a6b7f8d7d4d582b5838b6ad10655a
Varal7 <varaal@gmail.com>
2016-06-22 11:13:05 +0200

    Add gulp task to compile jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
+
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('jade', function() {
+    return gulp.src('client/src/*.jade')
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest('client/www'))
 });
